dd1a7fbf982450392bb01de9eff8ad0122c472a8
Johnson Wang <swanxinjiang@gmail.com>
2017-09-09 01:14:47 +0800

    tensor2 and tensor3 transformation matrices for force constants are implemented in symmetry

--- mdfc/symmetry.py
+++ mdfc/symmetry.py
@@ -1,6 +1,8 @@
-import spglib
 import numpy as np
+import spglib
 from phonopy.structure.cells import Primitive
+from itertools import permutations
+from mdfc.fcmath import similarity_transformation
 class Symmetry():
     def __init__(self, cell, symprec=1e-6):
         self.cell = cell
@@ -8,6 +10,8 @@ class Symmetry():
         self.symprec = symprec
         self.rot_inv = None
         self.rot_mult = None
+        self.tensor2 = None
+        self.tensor3 = None
         self.site_symmetries = {}
         self.symmetry_operations = \
             spglib.get_symmetry(cell=cell, symprec=symprec)
@@ -36,29 +40,19 @@ class Symmetry():
         # to ensure the first element is the I matrix
         self.pointgroup_operations = pointgroup_operations
 
-    def get_atom_mapping_under_sitesymmetry(self, center_atom, atom):
+    def get_atom_mapping_under_sitesymmetry(self, center_atom, atom, symmetries=None):
         """atom is mapped to a lower(or equal) index under the sitesymmetry at center_atom"""
-        symmetries = self.get_site_symmetry(center_atom)
+        if symmetries is None:
+            symmetries = self.get_site_symmetry(center_atom)
         positions = self.positions - self.positions[center_atom]
         if len(symmetries) == 1: # speeding up the case of no symmetries
             return atom, 0
         p = positions[atom]
-        pos2 = self.pointgroup_operations[symmetries].dot(p)
-        diff = pos2[np.newaxis] - positions[:atom+1, np.newaxis]
+        pos2 = self.pointgroup_operations[symmetries].dot(p) # shape: [nsym, 3]
+        diff = pos2[np.newaxis] - positions[:atom+1, np.newaxis] # shape[iatom+1, nsym,3]
         diff -= np.rint(diff)
-        return zip(*np.where(np.all(np.abs(diff) < self.symprec, axis=-1)))[0]
-        # positions[:atom+1]
-        # for j in range(atom+1):
-        #     diff = self.pointgroup_operations[symmetries].dot(p) - positions[j]
-        #     diff -= np.rint(diff)
-        #     np.all(np.abs(diff) < self.symprec, axis=1)
-        #     if np.all(np.abs(diff) < self.symprec, axis=1).any():
-        #         return j, np.where()
-        #     for k in symmetries:
-        #         r = self.pointgroup_operations[k]
-        #         if np.allclose(np.dot(r, p), positions[j], atol=self.symprec):
-        #             return j, k
-
+        iatoms, isyms = np.where(np.all(np.abs(diff) < self.symprec, axis=-1))
+        return iatoms[0], symmetries[isyms[0]]
 
     def set_multiplication_table(self):
         num_rot = len(self.pointgroup_operations)
@@ -68,9 +62,6 @@ class Symmetry():
             for j, pg2 in enumerate(self.pointgroup_operations):
                 pg = np.dot(pg1, pg2)
                 k = np.all(pg==self.pointgroup_operations, axis=(1,2)).argmax()
-                # for k, pg3 in enumerate(self.pointgroup_operations):
-                #     if (pg == pg3).all():
-                #         break
                 self.rot_mult[i, j] = k
                 if k == 0: # rot1.dot.rot2 == I
                     self.rot_inv[i] = j
@@ -127,6 +118,49 @@ class Symmetry():
                 bond_sym.append(i)
         return np.intc(bond_sym)
 
+    def get_site_symmetry_at_atoms(self,
+                                   atoms):
+        """
+        Site symmetry at atoms (either 1 or multiple atoms)
+        """
+
+        if atoms == None:
+            return np.arange(len(self.pointgroup_operations)).astype('intc')
+        elif type(atoms) in [list, tuple, np.ndarray]:
+            atom_center = atoms[0]
+            symmetries = self.get_site_symmetry(atom_center)
+            rotations = self.pointgroup_operations[symmetries]
+            pos = self.positions - self.positions[atom_center]
+            diff = np.array([np.dot(rotations, pos[atom]) - pos[atom] for atom in atoms]) # shape[natoms, nsym, 3]
+            diff -= np.rint(diff)
+            indices = np.where(np.all(np.abs(diff) < self.symprec, axis=(0,2))) # over natoms and direction
+            return symmetries[indices]
+        else: # atoms is an integer
+            return self.get_site_symmetry(atoms)
+
+    def set_tensor2(self):
+        lattice = self.cell.get_lattice().T
+        self.tensor2 = np.zeros((len(self.pointgroup_operations)*2, 9, 9), dtype='double')
+        nopes = len(self.pointgroup_operations)
+        for i, rot in enumerate(self.pointgroup_operations):
+            rot_cart = similarity_transformation(lattice, rot)
+            tensor2 = np.kron(rot_cart, rot_cart)
+            self.tensor2[i] = tensor2
+            self.tensor2[i+nopes] = tensor2.reshape(3,3,9).swapaxes(0,1).reshape(9,9)
+
+    def set_tensor3(self):
+        """Get the transformation tensor3 of 3rd anharmonic force constants
+        Rot.Perm(Phi) = Psi*, where Phi* is the known one.
+        """
+        lattice = self.cell.get_lattice().T
+        nopes = len(self.pointgroup_operations)
+        self.tensor3 = np.zeros((nopes*6, 27,27), dtype='double')
+        for i, rot in enumerate(self.pointgroup_operations):
+            rot_cart = similarity_transformation(lattice, rot)
+            tensor3 = np.kron(np.kron(rot_cart, rot_cart), rot_cart).reshape(3,3,3,27)
+            for j, perm in enumerate(permutations("ijk")):
+                self.tensor3[i+j*nopes] = np.einsum("ijkl->%sl"%perm, tensor3).reshape(27,27)
+
     def get_all_operations_at_star(self, atom):
         star = self.mapping[atom]
         map_operations = []
@@ -163,78 +197,3 @@ class Symmetry():
             if (abs(diff - np.rint(diff)) < symprec).all():
                 map_operations.append(nrot)
         return np.intc(map_operations)
-
-# def get_atom_sent_by_operation(orig_atom, positions, r, t, symprec=1e-5):
-#     rot_pos = np.dot(positions[orig_atom], r.T) + t
-#     diff = positions - rot_pos
-#     atom2 = np.where(np.all(np.abs(diff - np.rint(diff)) < symprec, axis=1))[0][0]
-#     return atom2
-#
-#
-# def get_atom_sent_by_site_sym(atom, center, positions, rot, symprec=1e-5):
-#     rot_pos = np.dot(positions[atom] - positions[center], rot.T) + positions[center]
-#     diff = positions - rot_pos
-#     atom2 = np.where(np.all(np.abs(diff - np.rint(diff)) < symprec, axis=1))[0][0]
-#     return atom2
-
-
-# def get_operations_at_star(operations, positions, atom, mappings, symprec):
-#     star = mappings[atom]
-#     map_operations = []
-#     for j, (r, t) in enumerate(
-#             zip(operations['rotations'], operations['translations'])):
-#         diff = np.dot(positions[atom], r.T) + t - positions[star]
-#         if (abs(diff - np.rint(diff)) < symprec).all():
-#             map_operations.append(j)
-#     return map_operations
-
-
-# def get_all_operations_at_star(rotations, translations, positions, atom, mappings, symprec):
-#     star = mappings[atom]
-#     map_operations = []
-#     for j, (r, t) in enumerate(
-#             zip(rotations, translations)):
-#         diff = np.dot(positions[atom], r.T) + t - positions[star]
-#         if (abs(diff - np.rint(diff)) < symprec).all():
-#             map_operations.append(j)
-#     return map_operations
-#
-#
-# def get_rotations_at_star(site_symmetries, positions, center_atom, atom, mappings, symprec):
-#     star = mappings[atom]
-#     map_operations = []
-#     rel_pos = positions - positions[center_atom]
-#     for j, r in enumerate(site_symmetries):
-#         diff = np.dot(rel_pos[atom], r.T) - rel_pos[star]
-#         if (abs(diff - np.rint(diff)) < symprec).all():
-#             map_operations.append(j)
-#     return site_symmetries[map_operations]
-
-
-# def get_next_atom(center, site_symmetry, positions, symprec=1e-5):
-#     """next_atom: a dict which should at least contain the key 'atom_number'
-#     site_symmetry: the site_symmetry at the center atom (atom_number)"""
-#     next_atom = {}
-#     next_atom['atom_number'] = center
-#     rela_pos = positions - positions[center]
-#     map_atoms = np.arange(len(positions))
-#     map_ops = np.zeros(len(positions), dtype=int)
-#     next_atom['site_symmetry'] = site_symmetry
-#     for i, p in enumerate(rela_pos):
-#         is_found = False
-#         for j in range(i):
-#             for k,r in enumerate(site_symmetry):
-#                 diff = np.dot(p, r.T) - rela_pos[j]
-#                 diff -= np.rint(diff)
-#                 if (abs(diff) < symprec).all():
-#                     map_atoms[i] = j
-#                     map_ops[i] = k
-#                     is_found = True
-#                     break
-#             if is_found:
-#                 break
-#     next_atom['mapping'] =map_atoms
-#     next_atom['mapping_operation'] = map_ops
-#     next_atom['independent_atoms'] = np.unique(map_atoms)
-#     next_atom['next_atoms'] = [{"atom_number":a} for a in np.unique(map_atoms)]
-#     return next_atom
